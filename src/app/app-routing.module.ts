12d2b147fe70d4a4b252ffdc6ec6941606eb07b1
Rohit <rohit.g@aalekhinsights.com>
2021-07-14 11:48:37 +0530

    routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddUserComponent } from './add-user/add-user.component';
+import { UserListComponent } from './user-list/user-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"userlist",
+    component:UserListComponent
+  },
+  {
+    path:"adduser",
+    component:AddUserComponent
+  },
+  {
+    path:"",
+    pathMatch:'full',
+    redirectTo:"userlist"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
