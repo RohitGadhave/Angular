b6ca5744c4c21359af817b7e34c686a04f71d5f9
Rohit <rohit.g@aalekhinsights.com>
2021-07-14 11:42:58 +0530

    user component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,15 @@ import { AppComponent } from './app.component';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { AddUserComponent } from './add-user/add-user.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddUserComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule,
