8e0a08b1839a3951bf276ff369bbb246a407180c
Rohit <rohit.g@aalekhinsights.com>
2021-07-14 17:58:33 +0530

    user component

--- src/app/add-user/add-user.component.ts
+++ src/app/add-user/add-user.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UserService } from '../services/user.service';
 @Component({
   selector: 'app-add-user',
   templateUrl: './add-user.component.html',
   styleUrls: ['./add-user.component.sass']
 })
 export class AddUserComponent implements OnInit {
+  imageFile: File = null;
+  model: any = {};
 
-  constructor() { }
+  constructor(
+    private userService: UserService
+  ) { }
 
   ngOnInit(): void {
+
+  }
+
+  onFormSubmit() {
+    console.warn(this.model);
+
+    if(!this.imageFile){
+      console.log("add img")
+      return false;
+    }
+    this.userService.addUser(this.model,this.imageFile).subscribe(
+      res=>{
+        console.log(JSON.stringify(res));
+      },
+      err=>{
+        console.log(JSON.stringify(err));
+
+      }
+    );
+  }
+
+  onFileSelected(event) {
+    console.warn(event);
+    this.imageFile = event.target.files[0];
   }
 
 }
